c72bba89da9ba067ca6c5f01dc9c4a858eab0eea
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-25 01:36:44 +0630

    feat: add second section

---
+++ components/CardView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mt-5 mb-4">
+    <h1>{{ largeCards.title }}</h1>
+    <p>{{ largeCards.snippet }}</p>
+    <b-row class="mt-5" cols="3">
+      <b-col
+        v-for="card in largeCards.cards"
+        :key="card.id"
+      >
+        <b-card
+          :title="card.title"
+          img-alt="Image"
+          :img-src="require(`~/assets/images/fires/images/${card.image}`)"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>{{ card.snippet }}</b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardView',
+  props: ['largeCards']
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-container>
+    <hero-page></hero-page>
+    <card-view
+      v-for="(largeCards,idx) in largeCards"
+      :key="idx"
+      :largeCards="largeCards"
+    />
+  </b-container>
+</template>
+
+<script>
+import HeroPage from '~/components/Hero'
+import { largeCardSections } from '~/assets/data'
+
+export default {
+  name: 'IndexPage',
+  components: { HeroPage },
+  data: () => {
+    return {
+      largeCards: largeCardSections
+    }
+  }
+}
+</script>
