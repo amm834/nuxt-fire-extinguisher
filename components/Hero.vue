f8d2dfb5e4a31ceafffc3bce5ec415ebfc19d158
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-09-24 18:47:48 +0630

    feat:add hero

---
+++ components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-row class="mt-5" cols="2">
+    <b-col>
+      <h1 class="font-weight-bolder">Find your fire extinguisher</h1>
+      <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi autem cupiditate, dignissimos
+        ea enim esse
+        eveniet excepturi, illo ipsa iure mollitia nemo non praesentium, quis quod repellat totam ut!</p>
+    </b-col>
+    <b-col>
+      <img class="w-50 ml-5" src="~/assets/images/fire-extingusher.svg" alt="">
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'HeroPage'
+}
+</script>
+
+<style scoped>
+
+</style>
